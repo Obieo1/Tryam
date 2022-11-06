4b637ae5f66c513899a936f8122d28520913d0eb
Adu Sarah <adusarah@Adus-Mackbook.local>
2022-11-06 15:53:53 +0100

    Testing

---
+++ src/components/Home.js
@@ -0,0 +1,59 @@
+import { useReducer } from "react";
+
+const ACTION = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+  RESET: "reset",
+  NEW_USER_INPUT: "newUserInput",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTION.INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case ACTION.DECREMENT:
+      return { ...state, count: state.count - 1 };
+    case ACTION.RESET:
+      return { ...state, count: (state.count = 0) };
+    case ACTION.NEW_USER_INPUT:
+      return { ...state, userInput: action.payload };
+    default:
+      throw new Error();
+  }
+};
+
+function Home() {
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    userInput: "",
+    color: false,
+  });
+
+  return (
+    <main
+      className="App"
+      style={{ color: state.color ? "#BF40BF" : "#A020F0." }}
+    >
+      <input
+        type="text"
+        value={state.userInput}
+        onChange={(e) =>
+          dispatch({ type: ACTION.NEW_USER_INPUT, payload: e.target.value })
+        }
+      />
+      <p>{state.count}</p>
+      <section>
+        <button onClick={() => dispatch({ type: ACTION.DECREMENT })}>
+          Decrement
+        </button>
+        <button onClick={() => dispatch({ type: ACTION.RESET })}>Reset</button>
+        <button onClick={() => dispatch({ type: ACTION.INCREMENT })}>
+          Increment
+        </button>
+      </section>
+      <p>{state.userInput}</p>
+    </main>
+  );
+}
+
+export default Home;
---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <>
+      <h1 className="err">Error 404 - Page Not Found</h1>
+      <p className="page">
+        The page you looking for does not exist, please kindly redirect.
+      </p>
+    </>
+  );
+};
+
+export default NotFound;
