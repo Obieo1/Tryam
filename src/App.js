358a305e3a64417ed8df9133dfbf681c601665c7
Adu Sarah <adusarah@Adus-Mackbook.local>
2022-11-06 17:17:07 +0100

    Hmm

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Errorboundaries from "./components/Errorboundaries";
 // import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
-    <>
-      <Errorboundaries>
-        <Routes>
+    <Errorboundaries>
+        <Routes basename="/Tryam">
           <Route path="/" element={<Home />}></Route>
         </Routes>
       </Errorboundaries>
-    </>
   );
 };
 
